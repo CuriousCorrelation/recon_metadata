acc888dc7456a9382de4ad9a2b39ff17d913689a
CuriousCorrelation <CuriousCorrelation@protonmail.com>
2021-08-06 01:12:39 +0530

    Add basic docs and initial use-case to `lib`

---
+++ src/lib.rs
@@ -0,0 +1,65 @@
+/*!
+# Usage
+
+## Basic usage
+
+### ISBN search
+
+```
+#[tokio::main]
+async fn main() {
+    use recon_metadata::Metadata;
+    use std::str::FromStr;
+
+    let isbn = isbn::Isbn::from_str("9781534431003").unwrap();
+    let resp = Metadata::from_isbn(&isbn)
+        .source(Source::default())
+        .await;
+
+    assert!(resp.is_ok())
+}
+```
+
+### Descriptive search
+
+```
+#[tokio::main]
+async fn main() {
+    use recon_metadata::Metadata;
+    use std::str::FromStr;
+
+    let description = "This is how you lose the time war";
+    let resp = Metadata::from_description(&description)
+        .search_provider(SearchProvider::default())
+        .source(Source::default())
+        .await;
+
+    assert!(resp.is_ok())
+}
+```
+
+*/
+
+pub mod interface;
+pub mod sources;
+pub mod types;
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+
+    // #[tokio::main]
+    // fn parses_metadata_from_isbn() {
+    //     use recon_metadata::Metadata;
+    //     use std::str::FromStr;
+    //
+    //     let isbn = isbn::Isbn::from_str("9781534431003").unwrap();
+    //     let resp = Metadata::from_isbn(&isbn).source(Source::default()).await;
+    //
+    //     assert!(resp.is_ok())
+    // }
+}
