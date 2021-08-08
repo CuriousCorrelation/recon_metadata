33d7fc4729e8d1f641683eddd77f95a268d869c6
CuriousCorrelation <CuriousCorrelation@protonmail.com>
2021-08-08 15:05:03 +0530

    Add `Message` to `ReconError` for unexpected API response

--- src/interface/recon.rs
+++ src/interface/recon.rs
@@ -3,13 +3,19 @@ use std::{error, fmt};
 #[derive(Debug)]
 /// A wrapper around errors raised by parsing libraries used in `book_recon_metadata`
 pub enum ReconError {
-    /// A wrapper around [`serde_json::Error`] typically raised by `serde_json::from_str/value`
+    /// Message in case of unexpected API response.
+    Message(String),
+    /// A wrapper around [`serde_json::Error`]
+    /// typically raised by `serde_json::from_str/value`
     JSONParse(serde_json::Error),
-    /// A wrapper around [`reqwest::Error`] typically raised by `reqwest::get(url)`
+    /// A wrapper around [`reqwest::Error`]
+    /// typically raised by `reqwest::get(url)`
     Connection(reqwest::Error),
-    /// A wrapper around [`isbn::IsbnError`] typically raised by `isbn::Isbn::from_str(possible_isbn_str)`
+    /// A wrapper around [`isbn::IsbnError`]
+    /// typically raised by `isbn::Isbn::from_str(possible_isbn_str)`
     ISBNParse(isbn::IsbnError),
-    /// A wrapper around [`chrono::ParseError`] typically raised by `NaiveDate::parse_from_str(&string, &format_Str)`
+    /// A wrapper around [`chrono::ParseError`]
+    /// typically raised by `NaiveDate::parse_from_str(&string, &format_Str)`
     DateParse(chrono::ParseError),
 }
 
