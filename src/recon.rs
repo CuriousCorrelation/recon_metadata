85f0760d6ea76e2524a5f162f7980c98d1b2e969
CuriousCorrelation <CuriousCorrelation@protonmail.com>
2021-08-09 20:53:14 +0530

    Add `Source` and `SearchProvider` enum

--- src/recon.rs
+++ src/recon.rs
@@ -1,5 +1,65 @@
 use std::{error, fmt};
 
+/// A list of search providers.
+/// Search providers are API to provide search functionality
+/// This is the first API call in `recon_metadata`
+#[derive(Debug)]
+pub enum SearchProvider {
+    GoogleBooks,
+    OpenLibrary,
+    Goodreads,
+    Amazon,
+}
+
+impl Default for SearchProvider {
+    fn default() -> Self {
+        SearchProvider::GoogleBooks
+    }
+}
+
+/// A list of sources.
+/// Sources are the book information providers
+/// where `recon_metadata` parses search results
+/// from [`SearchProvider`] and gathers additional
+/// information on it via these souces.
+#[derive(Debug)]
+pub enum Source {
+    GoogleBooks,
+    OpenLibrary,
+    Goodreads,
+    Amazon,
+}
+
+impl Default for Source {
+    fn default() -> Self {
+        Source::GoogleBooks
+    }
+}
+
+impl Source {
+    pub fn all() -> Vec<Self> {
+        vec![
+            Self::GoogleBooks,
+            Self::OpenLibrary,
+            Self::Goodreads,
+            Self::Amazon,
+        ]
+    }
+
+    pub fn add(sources: &mut Vec<Self>, source: Self) -> &Vec<Self> {
+        sources.push(source);
+        sources
+    }
+}
+
+/// A config struct. Provides configuration information like sources and search providers.
+#[derive(Debug)]
+pub struct ReconSetup {
+    pub(crate) search_provider: SearchProvider,
+    pub(crate) sources:         Vec<Source>,
+    pub(crate) extra:           bool,
+}
+
 #[derive(Debug)]
 /// A wrapper around errors raised by parsing libraries used in `book_recon_metadata`
 pub enum ReconError {
