bb651b528d3fc25496f35be437eeecb255782585
CuriousCorrelation <CuriousCorrelation@protonmail.com>
2021-08-09 20:56:35 +0530

    Refactor `base` type impl for general return type in `adaptor`

--- src/types/base.rs
+++ src/types/base.rs
@@ -2,13 +2,18 @@ use crate::recon::ReconError;
 use chrono::NaiveDate;
 use isbn::Isbn;
 
-pub type ISBNs = Vec<Result<Isbn, ReconError>>;
-pub type Titles = Vec<Result<String, ReconError>>;
-pub type Authors = Vec<Result<String, ReconError>>;
-pub type Descriptions = Vec<Result<String, ReconError>>;
-pub type Publishers = Vec<Result<String, ReconError>>;
-pub type PublicationDates = Vec<Result<NaiveDate, ReconError>>;
-pub type Languages = Vec<Result<String, ReconError>>;
-pub type Tags = Vec<Result<String, ReconError>>;
-pub type PageCount = Vec<Result<u16, ReconError>>;
-pub type CoverImages = Vec<Result<String, ReconError>>;
+pub type Generic = Result<String, ReconError>;
+pub type Numeric = Result<u16, ReconError>;
+pub type ISBN = Result<Isbn, ReconError>;
+pub type Date = Result<NaiveDate, ReconError>;
+
+pub type ISBNs = Vec<ISBN>;
+pub type Titles = Vec<Generic>;
+pub type Authors = Vec<Generic>;
+pub type Descriptions = Vec<Generic>;
+pub type Publishers = Vec<Generic>;
+pub type PublicationDates = Vec<Date>;
+pub type Languages = Vec<Generic>;
+pub type Tags = Vec<Generic>;
+pub type PageCount = Vec<Numeric>;
+pub type CoverImages = Vec<Generic>;
