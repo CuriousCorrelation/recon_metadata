a135c8e35a9b8824d9e41c837d5619c1ba247983
CuriousCorrelation <CuriousCorrelation@protonmail.com>
2021-09-07 16:12:11 +0530

    Add docs for `translater` mod

--- src/util/translater.rs
+++ src/util/translater.rs
@@ -11,6 +11,11 @@ use std::{
     str::FromStr,
 };
 
+/// Helper function that takes an [`Option`] value and converts it into an [`HashSet`]
+/// by mapping [`None`] to empty [`HashSet`] and [`Some`] to an inserted element.
+/// `Metadata` struct contains a [`HashSet`] for each of its fields
+/// where incomplete or unavailable information is avoided in-favor of information
+/// from more sources defined in `Source` struct in `recon.rs`.
 pub(crate) fn optional_to_hashset<T: std::hash::Hash + std::cmp::Eq>(
     value: Option<T>,
 ) -> HashSet<T> {
@@ -24,6 +29,8 @@ pub(crate) fn optional_to_hashset<T: std::hash::Hash + std::cmp::Eq>(
     }
 }
 
+/// A fallback function for [`HashSet`]. Works on the same principle as `optional_to_hashset`
+/// mapping [`Some`] to the value inside and [`None`] to an empty container.
 pub(crate) fn hashset_fallback<T: std::hash::Hash + std::cmp::Eq>(
     value: Option<HashSet<T>>,
 ) -> HashSet<T> {
@@ -33,22 +40,47 @@ pub(crate) fn hashset_fallback<T: std::hash::Hash + std::cmp::Eq>(
     }
 }
 
-pub(crate) fn number(n: Option<u16>) -> HashSet<u16> {
-    optional_to_hashset(n)
-}
-
 pub(crate) fn empty() -> HashSet<String> {
     HashSet::new()
 }
 
+/// Following functions translate `serde` values into values compatible with `Metadata` field.
+/// "..." means 'doesn't matter' or handled by `serde` `deserialize` implementation elsewhere.
+
+/// Example use-case:
+/// { "...": 42 } -> Serde { 42 } -> [42]
+pub(crate) fn number(n: Option<u16>) -> HashSet<u16> {
+    optional_to_hashset(n)
+}
+
+/// Example use-case:
+/// { "...": "some string" } -> Serde { "some string" } -> ["some string"]
 pub(crate) fn string(s: Option<String>) -> HashSet<String> {
     optional_to_hashset(s)
 }
 
+/// Example use-case:
+/// { "...": ["some string", "some other string", "some string"] }
+///   -> Serde { ["some string", "some other string", "some string"] }
+///   -> ["some string", "some other string"]
 pub(crate) fn vec(vec: Option<Vec<&str>>) -> HashSet<String> {
     hashset_fallback(vec.map(|vec| vec.into_iter().map(|v| v.to_owned()).collect()))
 }
 
+/// Example use-case:
+/// { "...":
+///    [
+///       { "...": "value1" }
+///       { "...": "value2" }
+///    ],
+///    [
+///       { "...": "value3" }
+///       { "...": "value4" }
+///       { "...": "value5" }
+///    ],
+///    ...
+/// }
+///   -> ["value1", "value2", "value3", "value4", "value5"]
 pub(crate) fn vec_hashmap_field(
     vec_hashmap: Option<Vec<HashMap<&str, &str>>>,
     field: &str,
@@ -63,6 +95,34 @@ pub(crate) fn vec_hashmap_field(
     }))
 }
 
+/// Function call: translater::vec_hashmap_field_split(opt_vec_hmap, "name"),
+/// Example use-case:
+///
+/// "...":
+///   [
+///      {
+///        "name": "science fiction",
+///        "url": "...",
+///        "...": "..."
+///      },
+///      {
+///        "name": "time-traveling",
+///        "url": "...",
+///        "...": "..."
+///      },
+///      {
+///        "name": "epistolary",
+///        "url": "...",
+///        "...": "..."
+///      },
+///      {
+///        "name": "Fiction, science fiction, general",
+///        "url": "...",
+///        "...": "..."
+///      }
+///   ]
+///
+///   -> ["science-fiction", "time-traveling", "epistolary", "fiction", "general"]
 pub(crate) fn vec_hashmap_field_split(
     vec_hashmap: Option<Vec<HashMap<&str, &str>>>,
     field: &str,
@@ -82,6 +142,19 @@ pub(crate) fn vec_hashmap_field_split(
     }))
 }
 
+/// Example use-case:
+///
+/// "...":
+///   {
+///      "isbn_10":
+///      [
+///        "isbn1",
+///        "isbn2"
+///      ],
+///      "...": "..."
+///   }
+///
+///   -> [Isbn10(isbn1), Isbn10(isbn2)]
 pub(crate) fn openlibrary_isbn10(
     hashmap_vec: &Option<HashMap<&str, Vec<&str>>>,
 ) -> HashSet<Isbn10> {
@@ -97,6 +170,19 @@ pub(crate) fn openlibrary_isbn10(
     }))
 }
 
+/// Example use-case:
+///
+/// "...":
+///   {
+///      "isbn_13":
+///      [
+///        "isbn1",
+///        "isbn2"
+///      ],
+///      "...": "..."
+///   }
+///
+///   -> [Isbn13(isbn1), Isbn13(isbn2)]
 pub(crate) fn openlibrary_isbn13(
     hashmap_vec: &Option<HashMap<&str, Vec<&str>>>,
 ) -> HashSet<Isbn13> {
@@ -112,6 +198,24 @@ pub(crate) fn openlibrary_isbn13(
     }))
 }
 
+/// Example use-case:
+///
+/// "...":
+///   {
+///     "small": "a.jpg",
+///     "medium": "b.jpg",
+///     "large": "c.jpg"
+///   }
+///
+///   -> [`CoverImage`]
+///   {
+///    small_thumbnail: [],
+///    thumbnail:       [],
+///    small:           ["a.jpg"],
+///    medium:          ["b.jpg"],
+///    large:           ["c.jpg"],
+///    extra_large:     [],
+///   }
 pub(crate) fn openlibrary_cover_images(hashmap: Option<HashMap<&str, &str>>) -> CoverImage {
     hashmap
         .map(|mut hashmap| {
@@ -162,6 +266,21 @@ pub(crate) fn openlibrary_cover_images(hashmap: Option<HashMap<&str, &str>>) -> 
         .unwrap_or_default()
 }
 
+/// Example use-case:
+///
+/// "...":
+///   [
+///     {
+///       "type": "ISBN_13",
+///       "identifier": "isbn13"
+///     },
+///     {
+///       "type": "ISBN_10",
+///       "identifier": "isbn10"
+///     }
+///   ],
+///
+///   -> Isbn10(isbn10)
 pub(crate) fn googlebooks_isbn10(
     hashmap_vec: &Option<Vec<HashMap<&str, &str>>>,
 ) -> HashSet<Isbn10> {
@@ -177,6 +296,23 @@ pub(crate) fn googlebooks_isbn10(
     }))
 }
 
+/// Example use-case:
+///
+/// "...":
+///   {
+///     "smallThumbnail": "a.jpg",
+///     "extraLarge": "b.jpg"
+///   }
+///
+///   -> [`CoverImage`]
+///   {
+///    small_thumbnail: ["a.jpg"],
+///    thumbnail:       [],
+///    small:           [],
+///    medium:          [],
+///    large:           [],
+///    extra_large:     ["b.jpg"],
+///   }
 pub(crate) fn googlebooks_cover_images(hashmap: Option<HashMap<&str, &str>>) -> CoverImage {
     hashmap
         .map(|mut hashmap| {
@@ -241,6 +377,21 @@ pub(crate) fn googlebooks_cover_images(hashmap: Option<HashMap<&str, &str>>) -> 
         .unwrap_or_default()
 }
 
+/// Example use-case:
+///
+/// "...":
+///   [
+///     {
+///       "type": "ISBN_13",
+///       "identifier": "isbn13"
+///     },
+///     {
+///       "type": "ISBN_10",
+///       "identifier": "isbn10"
+///     }
+///   ],
+///
+///   -> Isbn13(isbn13)
 pub(crate) fn googlebooks_isbn13(
     hashmap_vec: &Option<Vec<HashMap<&str, &str>>>,
 ) -> HashSet<Isbn13> {
@@ -256,6 +407,19 @@ pub(crate) fn googlebooks_isbn13(
     }))
 }
 
+/// Example use-case:
+///
+/// { "...": "2019-07-16" }
+///
+/// -> [NaiveDate(2019-07-16)]
+///
+/// { "...": "May 07 16" }
+///
+/// -> [NaiveDate(2016-05-07)]
+///
+/// { "...": "Not a date" }
+///
+/// -> []
 pub(crate) fn publication_date(s: Option<&str>) -> HashSet<NaiveDate> {
     let possible_formats = ["%B %d, %Y", "%Y-%m-%d", "%B, %d %Y"];
 
